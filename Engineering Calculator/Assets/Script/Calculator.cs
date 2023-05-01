36aaa532e8f4a3ebb5399cbd2c52d33f350aff5c
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-05-01 12:16:57 +0900

    er

---
+++ Engineering Calculator/Assets/Script/Calculator.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    class Graph
+    {
+        public void DrawGraph()
+        {
+
+        }
+
+        public double Calculate(string expression, double x)
+        {
+            return x;
+        }
+
+    }
+
+}
