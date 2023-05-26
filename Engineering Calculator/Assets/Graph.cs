16577d32eb5a045cd77101dd223acad0dfd54c9f
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-05-26 18:16:47 +0900

    최대한 함수형으로

--- Engineering Calculator/Assets/Graph.cs
+++ Engineering Calculator/Assets/Graph.cs
@@ -4,86 +4,158 @@ using System.Collections.Generic;
 using UnityEngine;
 using NCalc;
 using TMPro;
+using System.Linq;
 
 public class Graph : MonoBehaviour
 {
     public GameObject father;
     public GameObject Dot;
-    public TMP_InputField inputField;
-    public TMP_InputField X;
-    public TMP_Text text;
-    // Start is called before the first frame update
-    void Start()
+    public SerializableDict<TMP_InputField> serializableDict;
+    Dictionary<string, TMP_InputField> inputFields;
+    void Awake()
     {
+        inputFields = serializableDict.getDict();
     }
 
     void Update()
     {
     }
 
-    public void PushFunc(string s)
+    /// <summary>
+    /// 필드에 문자열을 추가하는 함수, 버튼에 사용됨
+    /// </summary>
+    /// <param name="s"></param>
+    public void PushFunc(string _str)
     {
-        inputField.text += s;
+        inputFields[InputFieldType.inputFieldFormula].text += _str;
     }
 
+    /// <summary>
+    /// 수식에 값을 반환함
+    /// </summary>
     public void Result()
     {
         try
         {
-            Expression exp = new Expression(inputField.text);
-            exp.Parameters["x"] = X.text;
-            text.text = exp.Evaluate().ToString();
+            var _InputField= inputFields[InputFieldType.inputFieldFormula];
+            Expression exp = new Expression(_InputField.text);
+            var xValue = inputFields[InputFieldType.inputFieldXValue].text;
+            exp.Parameters["x"] = xValue;
+            _InputField.text = exp.Evaluate().ToString();
         } catch (Exception e)
         {
             Debug.Log(e);
         }
     }
 
+    /// <summary>
+    /// 그래프를 수식으로 그려줌
+    /// </summary>
     public void PushButton()
     {
-        DeleteChild();
+        DeleteChild(father.transform);
         try
         {
-            DrawGraph(inputField.text);
-        }   catch (Exception e)
+            var inputField = inputFields[InputFieldType.inputFieldFormula];
+            DrawGraph(inputField.text, Dot);
+        } catch (Exception e)
         {
             Debug.Log(e);
         }
     }
 
-    public void DrawGraph(string str)
+    /// <summary>
+    /// 입력된 식을 그래프로 표현함
+    /// </summary>
+    /// <param name="str"></param>
+    /// <exception cref="Exception"></exception>
+    public void DrawGraph(string str, GameObject Dot)
     {
 
-        //if (str.IndexOf('^') != -1) throw new Exception("지원되지않는 연산자");
+        if (str.IndexOf('^') != -1) throw new Exception("지원되지않는 연산자");
         for (double i = -10; i < 10; i += 0.001f)
         {
             Expression exp = new(str);
             exp.Parameters["x"] = i;
-            Dotted(i, Convert.ToDouble(exp.Evaluate().ToString()));
+            var resultStr = exp.Evaluate().ToString();
+            var result = Convert.ToDouble(resultStr);
+            var ObjectPosition = new Vector3((float)i, (float)result, 0);
+            var DotObject = CreateGameObect(Dot);
+            DotObject.transform.position = ObjectPosition; 
+ 
+            
         }
     }
 
-    public void Dotted(double x, double y) => Instantiate(Dot, new Vector3((float)x, (float)y, 0), Quaternion.identity).transform.parent = father.transform;
+    /// <summary>
+    /// @object를 x, y 좌표에 생성합니다.
+    /// </summary>
+    /// <param name="object"></param>
+    /// <returns>원한 좌표에 생성된 오브젝트를 반환</returns>
+    public GameObject CreateGameObect(GameObject @object) => Instantiate(@object);
 
-    public void DeleteText()
+    /// <summary>
+    /// 트렌스폼에서 위치값을 추출합니다
+    /// </summary>
+    /// <param name="father"></param>
+    /// <returns>프지션</returns>
+    public Vector3 GetPosition(Transform father) => father.transform.position;
+
+
+    /// <summary>
+    /// 값 수식 대입값 을 지웁니다
+    /// </summary>
+    public void DeleteInputText()
     {
-        text.text = "";
-        inputField.text = "";
-        X.text = "";
+        foreach (var inputField in inputFields.Values) inputField.text = string.Empty;
     }
 
-    public void DeleteChild()
+    /// <summary>
+    /// 아버지 오브젝트 아래 자식들을 삭제합니다.
+    /// </summary>
+    /// <param name="parent"></param>
+    public void DeleteChild(Transform parent)
     {
-        Transform[] childList = father.GetComponentsInChildren<Transform>();
-        if (childList != null)
+        Transform[] childList = parent.GetComponentsInChildren<Transform>();
+
+        if (childList == null) return;
+
+        foreach (var child in childList)
         {
-            for (int i = 1; i < childList.Length; i++)
+            if (child == parent) continue;
+
+            Destroy(child.gameObject);
+        }
+
+    }
+
+    [Serializable]
+    public class SerializableDict<T>
+    {
+        public List<SerializeData<T>> data;
+        private Dictionary<string, T> dict = new();
+
+        public Dictionary<string, T> getDict()
+        {
+            for (int i = 0; i < data.Count; i++)
             {
-                if (childList[i] != transform)
-                {
-                    Destroy(childList[i].gameObject);
-                }
+                dict.Add(data[i].key, data[i].value);
             }
+            return dict;
         }
     }
+
+    [Serializable]
+    public class SerializeData<T>
+    {
+        public string key;
+        public T value;
+    }
+
+    static class InputFieldType
+    {
+        public static readonly string inputFieldFormula = "inputFieldRormula";
+        public static readonly string inputFieldXValue = "inputFieldXValue";
+    }
+
 }
