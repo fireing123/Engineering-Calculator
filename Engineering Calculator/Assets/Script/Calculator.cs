b3f1003939a960ae86dd177c0a2e0072051face6
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-05-31 15:26:21 +0900

    commit

--- Engineering Calculator/Assets/Script/Calculator.cs
+++ Engineering Calculator/Assets/Script/Calculator.cs
@@ -1,8 +1,5 @@
 using NCalc;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace GraphMake
 {
--- Engineering Calculator/Assets/Script/Graph.cs
+++ Engineering Calculator/Assets/Script/Graph.cs
@@ -165,6 +165,4 @@ public class Graph : MonoBehaviour
         public T value;
     }
 
-    
-
 }
