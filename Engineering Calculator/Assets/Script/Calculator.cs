6b40c1bc358fe961dc9a22640c9ed4788b1afc25
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-06-22 20:36:53 +0900

    주석달음

--- Engineering Calculator/Assets/Script/Calculator.cs
+++ Engineering Calculator/Assets/Script/Calculator.cs
@@ -7,6 +7,12 @@ namespace GraphMake
     public static class Calculator
     {
 
+        /// <summary>
+        /// Nclcu 를 사용해 str 값을 계산해 double 값으로 반환함
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="Xvalue"></param>
+        /// <returns></returns>
         public static double Calculate(string str, double Xvalue)
         {
             object Eval = NCalcu(str, Xvalue);
@@ -14,6 +20,12 @@ namespace GraphMake
             return ToDouble(result);
         }
 
+        /// <summary>
+        /// Nclac 라이브러리를 사용해 str 수식을 오브젝트로 반환함
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="Xvalue"></param>
+        /// <returns>계산돤 오브젝트</returns>
         public static object NCalcu(string str, double Xvalue)
         {
             Expression expression = new Expression(str);
@@ -21,11 +33,21 @@ namespace GraphMake
             return expression.Evaluate();
         }
 
+        /// <summary>
+        /// 값이 무한이거나 숫자가 아니면 false 를 반환함
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>value 가 숫자인가?</returns>
         public static bool HasValue(double value)
         {
             return !(double.IsNaN(value) || double.IsInfinity(value));
         }
 
+        /// <summary>
+        /// string 을 double 형으로 반환함
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>double</returns>
         public static double ToDouble(string str)
         {
             return Convert.ToDouble(str);
@@ -35,6 +57,11 @@ namespace GraphMake
 
     }
 
+    /// <summary>
+    /// 입력창 타입임
+    /// formula 는 수식창 
+    /// value는 x 값 창
+    /// </summary>
     public static class InputFieldType
     {
         public static string inputFieldFormula = "inputFieldFormula";
